5084c9ff9314f29174a8835baf1a67c7dbbd9a5b
Daniel Way <projects@danieldway.com>
2024-07-28 15:46:42 -0500

    Refactor crate so model structures are mapped by functions on the Archive II models

--- examples/data/README.md
+++ examples/data/README.md
@@ -2,12 +2,7 @@
 
 These binary files were produced from the `KDMX20220305_233003_V06` data file of an EF4 tornado near Des Moines, IA on 
 March 5, 2022. A bespoke script was used to decompress LDM records and isolate individual blocks of encoded data such as
-the Archive II file header, a message header, and a digital radar data message body.
-
-## `archive2_header`
-
-This is a sample of an Archive II file header. The header is 24 bytes long and contains information about the site and
-time the data was collected.
+a message header and a digital radar data message body.
 
 ## `message_header`
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 //!
 //! # nexrad-decode
 //! Decoding functions and models for NEXRAD weather radar data. Decoder and struct definitions are
-//! in accordance with NOAA's WSR-88D Interface Control Document for Archive II "ICD 2620010H".
+//! in accordance with NOAA's WSR-88D Interface Control Document for Archive II "ICD 2620010H"
+//! build 19.0.
 //!
 //! Optionally, the `nexrad-model` feature provides mappings to a common model for representing
 //! radar data. The `uom` feature can also be used to provide type-safe units of measure.
@@ -11,7 +12,7 @@
 #![warn(clippy::correctness)]
 #![allow(clippy::too_many_arguments)]
 
-pub mod raw;
-#[cfg(feature = "nexrad-model")]
-pub mod model;
+pub mod messages;
 pub mod result;
+
+mod util;
